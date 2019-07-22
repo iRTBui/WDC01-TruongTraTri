94058952053f115907222f4fc2c343345af18786
iRTBui <42140551+iRTBui@users.noreply.github.com>
2019-07-22 23:48:17 +0700

    Tri - Update number of Filps
    Update number of Filps

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,49 @@
+// khai báo 1 biến card và gán tất cả phần tử matching-card vào
 const cards = document.querySelectorAll('.matching-card');
 
+// biến có thẻ lật
 let hasFlippedCard = false;
+// lock board
 let lockBoard = false;
+// tạo 2 card
 let firstCard, secondCard;
+//
+let totalClick = 0;
 
+//hàm lật card
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
+  if(lockBoard) return;
+  if(this === firstCard) return;
+  // xử lý card lật qua lại, thêm flip vào class "matching-card flip"
   this.classList.add('flip');
-
-  if (!hasFlippedCard) {
-    // first click
+  this.ticker = document.getElementById('flips');
+  
+  if(!hasFlippedCard) {
+    // click thứ 1
     hasFlippedCard = true;
     firstCard = this;
 
     return;
   }
 
-  // second click
+  // click thứ 2
   secondCard = this;
 
+  //tăng số lượt lật
+  totalClick++;
+  this.ticker.innerText = totalClick;
+  //kiểm tra match
   checkForMatch();
+
 }
 
+// hàm kiếm tra card đã match
 function checkForMatch() {
   let isMatch = firstCard.dataset.match === secondCard.dataset.match;
-
-  isMatch ? disableCards() : unflipCards();
+  isMatch ? disableCards() : unFlipCard();
 }
 
+// hàm set card đã match
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
@@ -37,7 +51,8 @@ function disableCards() {
   resetBoard();
 }
 
-function unflipCards() {
+// hàm xóa flip để card quay về trạng thái ban đầu
+function unFlipCard() {
   lockBoard = true;
 
   setTimeout(() => {
@@ -45,17 +60,19 @@ function unflipCards() {
     secondCard.classList.remove('flip');
 
     resetBoard();
-  }, 1500);
+  }, 500);
 }
 
+// hàm reset nếu không trùng với firtCard
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+  [firstCard, secondCard] = [null, null]
 }
 
+// ngẫu nhiên cho các card
 (function shuffle() {
   cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
+    let randomPos = Math.floor(Math.random() * 16);
     card.style.order = randomPos;
   });
 })();
